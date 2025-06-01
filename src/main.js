c087729788f94bce94a4084410f389408ac51c9e
多啦b梦 <1715109585@qq.com>
2025-06-01 11:22:25 +0800

    完成markdown本地化

--- src/main.js
+++ src/main.js
@@ -38,3 +38,52 @@ try {
 } catch (error) {
   console.error('初始化主题失败:', error)
 }
+
+//## markdown editor
+import { config } from 'md-editor-v3';
+import 'md-editor-v3/lib/style.css';
+
+import screenfull from 'screenfull';
+
+import katex from 'katex';
+import 'katex/dist/katex.min.css';
+
+import Cropper from 'cropperjs';
+import 'cropperjs/dist/cropper.css';
+
+import mermaid from 'mermaid';
+
+import highlight from 'highlight.js';
+import 'highlight.js/styles/atom-one-dark.css';
+
+// <3.0
+// import prettier from 'prettier';
+// import parserMarkdown from 'prettier/parser-markdown';
+// >=3.0
+import * as prettier from 'prettier';
+import parserMarkdown from 'prettier/plugins/markdown';
+
+config({
+  editorExtensions: {
+    prettier: {
+      prettierInstance: prettier,
+      parserMarkdownInstance: parserMarkdown,
+    },
+    highlight: {
+      instance: highlight,
+    },
+    screenfull: {
+      instance: screenfull,
+    },
+    katex: {
+      instance: katex,
+    },
+    cropper: {
+      instance: Cropper,
+    },
+    mermaid: {
+      instance: mermaid,
+    },
+  },
+});
+//##
